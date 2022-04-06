02798742f505255343b164559cd4c7e775697e9d
Payden Dyer <63562333+paydendyer@users.noreply.github.com>
2022-04-06 00:06:49 -0500

    Added function that displays list of expenses

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,13 @@ const addForm = document.getElementById("addForm");
 const budgetAmount = document.getElementById("budgetAmount");
 const balanceAmount = document.getElementById("balanceAmount");
 
+const expForm = document.getElementById("expForm");
+let expName = document.getElementById("expName");
+let expNumber = document.getElementById("expNumber");
+
+let id = 0;
+let details = [];
+
 //Takes budget amount and displays it after user adds an amount
 function getBudgetAmount(amount) {
   if (!amount) {
@@ -23,6 +30,44 @@ function getBudgetAmount(amount) {
     amountInput.value = "";
   }
 }
+//Displays list of expenses
+function addExpenses(name, number) {
+  if(!name.length || !number.length) {
+    expName.style.border = "1px solid #b80c09";
+    expName.placeholder = "input can not be empty";
+    expName.style.color = "#b80c09";
+
+    expNumber.style.border = "1px solid #b80c09";
+    expNumber.placeholder = "input can not be empty";
+    expNumber.style.color = "#b80c09";
+
+    setTimeout(() => {
+      expName.style.color = "#495057";
+      expName.style.border = "1px solid gray";
+      expName.placeholder = "input can not be empty";
+
+      expNumber.placeholder = "input can not be empty";
+      expNumber.style.border = "1px solid gray";
+      expNumber.style.color = "#495057";
+    }, 3000);
+  } else {
+    const userExp = {
+      id: id,
+      name: name,
+      number: parseInt(number),
+    };
+    details.push(userExp);
+    displayExp(details);
+    id++;
+    expName.value = "";
+    expNumber.value = "";
+  }
+}
+
+expForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addExpenses(expName.value, expNumber.value);
+});
 
   addForm.addEventListener("submit", (e) => {
     e.preventDefault();
